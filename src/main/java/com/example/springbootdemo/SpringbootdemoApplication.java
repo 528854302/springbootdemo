5352b2d48097ccda6e5df0d4b38021604eb6ba4e
528854302 <46879923+528854302@users.noreply.github.com>
2019-09-28 13:14:24 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/springbootdemo/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+//    @GetMapping("hello")
+//    public String hello(@RequestParam(name = "name")String name, Model model){
+//        model.addAttribute("name",name);
+//        return "hello";
+//    }
+
+    @GetMapping("")
+    public String hello(){
+        return "demo";
+    }
+}
